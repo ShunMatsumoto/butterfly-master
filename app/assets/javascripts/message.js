68195837ed544414252a05a97cbbd425356b6f28
ShunMatsumoto <mr.bigmr.little@gmail.com>
2020-01-03 14:43:04 +0900

    automatitic_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,13 +1,13 @@
 $(function(){
 
-  function buildHTML(message){
+  var buildHTML = function(message) {
     var html = `<div class="message" data-message-id=${message.id}>
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                     ${message.user_name}
                     </div>
                     <div class="upper-message__date">
-                    ${message.date}
+                    ${message.created_at}
                     </div>
                   </div>
                   <div class="lower-message">
@@ -34,12 +34,44 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
+      $('#message_content').val('');
       $('.main__box__chat').animate({ scrollTop: $('.main__box__chat')[0].scrollHeight});
       $('.form__submit').prop('disabled', false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     });
-  })
+  });
+
+    var reloadMessages = function() {
+      last_message_id = $(".message:last").data("message-id");  //$('.message:last')jQueryのオブジェクトの指定方法の1つに、:lastがあります。今回の場合は.messageというクラスがつけられた全てのノードのうち一番最後のノード、という意味になる
+      $.ajax ({
+        url: "api/messages",          //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        type: "get",                  //ルーティングで設定した通りhttpメソッドをgetに指定
+        dataType: "json",
+        data: {id: last_message_id}   //dataオプションでリクエストに値を含める
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+        var insertHTML = "";           //追加するHTMLの入れ物を作る
+        $.each(messages, function(i, message) {     //配列messagesの中身を一つ一つ取り出し、HTMLに変換したものを入れ物に足し合わせる
+          insertHTML += buildHTML(message)
+        });
+        $(".messages").append(insertHTML);          //メッセージが入ったHTMLに入れ物毎追加
+        $('.main__box__chat').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#message_content").val('');
+        $(".form__submit").prop("disabled", false);
+      }
+      })
+      .fail(function() {
+        console.log('error');
+      })
+    }
+    if (document.location.href.match(/\/lessons\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);                              //setInterval(定期的動かしたい関数, ミリ秒単位で動かす感覚);
+    }
 });
+
+//matchメソッドの引数として書いている/\/lessons\/\d+\/messages/の部分が正規表現です。正規表現は基本的には/と/で囲んだ部分になりますが、/自体も正規表現に含めたい場合、直前に\(バックスラッシュ)を付けます。
+//また、\+d\の部分は、「桁無制限の数値」という意味になります。具体的には、dが0 ~ 9までの数字のどれかを表し、+は+のついた文字が何文字でもマッチする、という特殊な意味を持ちます。
+//これで、URLにgroups/数字/messagesという部分があるページでない限り、reloadMessagesメソッドが動くことはありません。
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -3,3 +3,4 @@ json.user_name @message.user.name
 json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
 json.content @message.content
 
+json.id @message.id  # 自動更新で使用するためidもデータとして渡す
